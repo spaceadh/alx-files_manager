53046387d2ce308d936a05047b07b2a7853b0d53
spaceadh <spacesealadhesives13@gmail.com>
2024-06-15 21:37:04 +0300

    AuthController

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import { Express } from 'express';
 import AppController from '../controllers/AppController';
-// import AuthController from '../controllers/AuthController';
+import AuthController from '../controllers/AuthController';
 import UsersController from '../controllers/UsersController';
 // import FilesController from '../controllers/FilesController';
 import { basicAuthenticate, xTokenAuthenticate } from '../middlewares/auth';
@@ -15,11 +15,11 @@ const injectRoutes = (api) => {
   api.get('/status', AppController.getStatus);
   api.get('/stats', AppController.getStats);
 
-  // api.get('/connect', basicAuthenticate, AuthController.getConnect);
+  api.get('/connect', basicAuthenticate, AuthController.getConnect);
   // api.get('/disconnect', xTokenAuthenticate, AuthController.getDisconnect);
-
+// 
   api.post('/users', UsersController.postNew);
-  // api.get('/users/me', xTokenAuthenticate, UsersController.getMe);
+  api.get('/users/me', xTokenAuthenticate, UsersController.getMe);
 
   // api.post('/files', xTokenAuthenticate, FilesController.postUpload);
   // api.get('/files/:id', xTokenAuthenticate, FilesController.getShow);
