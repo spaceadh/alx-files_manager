19ae6b878613dfcd7a0fbba48fcc5973db5b52ab
spaceadh <spacesealadhesives13@gmail.com>
2024-06-15 21:31:09 +0300

    server.js

--- server.js
+++ server.js
@@ -1,28 +1,12 @@
 import express from 'express';
+import startServer from './libs/boot';
 import injectRoutes from './routes';
-import envLoader from './utils/env_loader';
+import injectMiddlewares from './libs/middlewares';
 
-// Load environment variables
-envLoader();
+const server = express();
 
-const app = express();
+injectMiddlewares(server);
+injectRoutes(server);
+startServer(server);
 
-const injectMiddlewares = (app) => {
-  app.use(express.json({ limit: '200mb' }));
-};
-
-injectMiddlewares(app);
-
-// app.use(routes);
-injectRoutes(app);
-
-const startServer = (api) => {
-  envLoader();
-  const port = process.env.PORT || 5000;
-  const env = process.env.npm_lifecycle_event || 'dev';
-  api.listen(port, () => {
-    console.log(`[${env}] API has started listening at port:${port}`);
-  });
-};
-
-startServer(app);
+export default server;
